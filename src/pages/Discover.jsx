06106535b35a5be955f28bc08cbe81e555929343
Artem <artem.xolod.90@gmail.com>
2023-11-06 16:01:41 +0300

    Настройка отображение информирующих компонентов Error и Loading

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -28,7 +28,7 @@ const Discover = () => {
 
             <div className="flex flex-wrap sm:justify-start justify-center gap-8">
                 {
-                    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((song, i) => (
+                    data.map((song, i) => (
                         <SongCard song={song} key={song + i} />
                     ))
                 }
